9018666c5521e870647e1cb87d64784f334adcfb
Preslav Rachev <rachev.preslav@gmail.com>
2020-04-14 14:08:06 +0200

    Extend the example

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,40 @@
   export let name;
   let selectedOption;
 
+  let options = [
+    {
+      label: "Left Selection",
+      active: true,
+      image:
+        "https://images.unsplash.com/photo-1518057111178-44a106bad636?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      author: {
+        name: "Clay Banks",
+        link: "https://unsplash.com/@claybanks"
+      }
+    },
+    {
+      label: "Middle Selection",
+      image:
+        "https://images.unsplash.com/photo-1511920170033-f8396924c348?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      author: {
+        name: "Nathan Dumlao",
+        link: "https://unsplash.com/@nate_dumlao"
+      }
+    },
+    {
+      label: "Right Selection",
+      image:
+        "https://images.unsplash.com/photo-1581701663554-291c6c9e56d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=676&q=80",
+      author: {
+        name: "Shotlist",
+        link: "https://unsplash.com/@shotlist"
+      }
+    }
+  ];
+
   import SegmentedControl from "./core/ios/SegmentedControl.svelte";
+  import Card from "./core/common/Card.svelte";
+  import UnsplashAttribution from "./misc/UnsplashAtribution.svelte";
 </script>
 
 <style>
@@ -35,13 +68,33 @@
     to learn how to build Svelte apps.
   </p>
 
-  <SegmentedControl
-    bind:selectedOption
-    options={[{ label: 'A', active: true }, { label: 'B' }, { label: 'C' }]} />
-  <div style="display:flex">
-    {#if selectedOption && selectedOption.label}
-      <div>Selected option:</div>
-      <div>{selectedOption.label}</div>
-    {/if}
+  <SegmentedControl bind:selectedOption bind:options />
+
+  <div style="column-count:3">
+    {#each options as option}
+      <div style="display:flex; flex-direction: column">
+        {#if option.active === true}
+          <Card
+            imageSrc={option.image}
+            header={option.label}
+            title={option.label}
+            subtitle={option.label} />
+        {:else}
+          <div style="background-color: #d4d4d4; padding: 2rem">
+            <Card
+              imageSrc={option.image}
+              header={option.label}
+              title={option.label}
+              subtitle={option.label} />
+          </div>
+        {/if}
+        <div style="padding-top: 2rem">
+          <UnsplashAttribution
+            author={option.author.name}
+            link={option.author.link} />
+        </div>
+      </div>
+    {/each}
   </div>
+
 </main>
